3ba892b6de33b485232b6310444c18b5d7973884
vykan12 <fireemblemvids@gmail.com>
2017-01-07 23:56:26 -0500

    FE8_Ephase variable renaming and final cleanup

--- scripts/FE8_Ephase.lua
+++ scripts/FE8_Ephase.lua
@@ -2,32 +2,32 @@ function nextrng(r1, r2, r3)
   return AND(XOR(SHIFT(r3, 5), SHIFT(r2, -11), SHIFT(r1, -1), SHIFT(r2, 15)),0xFFFF)
 end
 
-BattleCheck = 1000;
-rngbase = 0x03000000;
-Phase = 0x0202BCFF;
-key1 = {};
-key1['A'] = true;
+battleLimit = 1000
+RNGBase = 0x03000000
+Phase = 0x0202BCFF
+key1 = {}
+key1['A'] = true
 
-StartF = movie.framecount();
-RNGCheck = savestate.create();
-savestate.save(RNGCheck);
+-- Create a script-only savestate
+RNGCheck = savestate.create()
+savestate.save(RNGCheck)
 
-for curord = 0, BattleCheck - 1, 1 do
-  savestate.load(RNGCheck);      
+for currentBattle = 0, battleLimit - 1, 1 do
+  savestate.load(RNGCheck)      
   
-  --RNGLOOP
-  for n = 1, curord, 1 do
-    Rtemp = nextrng(memory.readword(rngbase+4), memory.readword(rngbase+2), memory.readword(rngbase+0));
-    memory.writeword(rngbase+4, memory.readword(rngbase+2));
-    memory.writeword(rngbase+2, memory.readword(rngbase+0));
-    memory.writeword(rngbase+0, Rtemp);      
-  end;
+  -- RNG Loop
+  for i = 1, currentBattle, 1 do
+    Rtemp = nextrng(memory.readword(RNGBase + 4), memory.readword(RNGBase + 2), memory.readword(RNGBase + 0))
+    memory.writeword(RNGBase + 4, memory.readword(RNGBase + 2))
+    memory.writeword(RNGBase + 2, memory.readword(RNGBase + 0))
+    memory.writeword(RNGBase + 0, Rtemp)
+  end
 
-  --PhaseLoop
+  -- Phase Loop
   while memory.readbyte(Phase) == 128 do
-    joypad.set(1, key1); -- hold A
-    key1.start = (not key1.start) or nil; -- press start every two frames
-    emu.frameadvance();
-    gui.text(10,10, string.format('%d of %d done.', curord + 1, BattleCheck));
-  end; 
-end;
+    joypad.set(1, key1) -- hold A
+    key1.start = (not key1.start) or nil -- press start every two frames
+    emu.frameadvance()
+    gui.text(10,10, string.format('%d of %d done.', currentBattle + 1, battleLimit))
+  end
+end
